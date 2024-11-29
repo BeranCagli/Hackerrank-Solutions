520d5b7a25c8fef7ade853bc84948f991309beb3
Beran Çağlı <148129395+BeranCagli@users.noreply.github.com>
2024-11-29 23:59:02 +0300

    Create Q2.cpp

---
+++ Q2.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    int sum,i,j,k;
+    cin >> i >> j >> k;
+    sum = i+j+k;
+    cout << sum;
+       
+    return 0;
+}
